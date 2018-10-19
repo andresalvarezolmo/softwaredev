6e5089f80de6cae65ef9e260623b50c6b2f691af
xDrews <43134259+xDrews@users.noreply.github.com>
2018-10-19 11:44:15 +0100

    Se acaba en el lab

--- Control/src/Choice.java
+++ Control/src/Choice.java
@@ -1,16 +1,53 @@
+import java.util.Scanner;
 
 public class Choice {
 
-		
-	public static void Choice1 (){
 	
-		System.out.println("Choice 1 executed");
+	public static void GetYourGrade (){
+		do {
+		System.out.println("Get your grade has been executed");
+		System.out.println("Please, enter your grade in percentage:");
+		Scanner in = new Scanner(System.in);
+	    double Grade = in.nextDouble();
 		
+		
+		if (Grade>69 && Grade <100) {
+			System.out.println("You got an A");
+		}
+		else if (Grade>59 && Grade <100) {
+			System.out.println("You got an B");
+		}	
+		else if (Grade>49 && Grade <100) {
+			System.out.println("You got an C");
+		}
+		else if (Grade>39 && Grade <100  ) {
+			System.out.println("You got an D");
+		}	
+		else if (Grade<=40 && Grade >0) {
+			System.out.println("You got an E");
+		}
+		else if (Grade==0) {
+			break;
+		}
+			
+		else {
+			System.out.println("Invalid value, enter a valid one");	
+		}
 	}
+		while(true);
+		}
 	
-	public static void Choice2 (){
+	public static void TableValues (){
 		
-		System.out.println("Choice 2 executed");
+		System.out.println("Tables values method has been executed");
+		System.out.println("Please, enter two integer values (separated by a space and the first one smaller than the second one:");
+		Scanner in = new Scanner(System.in);
+		int lower=Integer.parseInt(in.next());
+		int upper=Integer.parseInt(in.next());
+		
+		for (int lower=0;lower==upper;lower++) {
+			
+		}
 		
 	}
 	
--- Control/src/Menu.java
+++ Control/src/Menu.java
@@ -5,17 +5,18 @@ import java.util.Scanner;
 
 public class Menu {
 	public void processUserChoices () {
-	do {		
-			displayMenu();
-			Scanner in = new Scanner(System.in);
-			String userChoice = in.nextLine();
+	do {
+		
+		displayMenu();
+		Scanner in = new Scanner(System.in);
+		String userChoice = in.nextLine();
 		
 		
 		if (userChoice.equals("1")) {
-			Choice.Choice1();
+			Choice.GetYourGrade();
 		}
 		else if (userChoice.equals("2")) {
-			Choice.Choice2();
+			Choice.TableValues();
 		}	
 		else if (userChoice.equals("3")) {
 			Choice.Choice4();
@@ -26,23 +27,30 @@ public class Menu {
 		
 		else {
 			System.out.println("Invalid value, enter a valid one");	
-			}
+		}
 		
 	} 
+	
 	while(true);
+	
 	}
 	
 	public void displayMenu(){
-	System.out.println("Please select one of the options below");
-	System.out.println("1. Option 1");
-	System.out.println("2. Option 2");
+	System.out.println("Please select one of the options below by typing the number");
+	System.out.println("1. Option 1 | Get your grade");
+	System.out.println("2. Option 2 | Table values");
 	System.out.println("3. Option 3");
 	System.out.println("4. Option 4");
-	System.out.println("exit");
+	System.out.println("5. Exit");
 	}
 		
 		
-
+	public void runMenu()
+	{
+		processUserChoices();
+	}
+	
+	
 	public static void main(String[] args) {
 		
 		Menu myMenu = new Menu();
@@ -50,9 +58,4 @@ public class Menu {
 	
 	}
 	
-	
-	public void runMenu()
-	{
-		processUserChoices();
-	}
 }
